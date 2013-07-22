19f043a52f4b65bac0c626a83d52af93a3d5a9e9
Rosa Garcia <rosaf.garcia@gmail.com>
2013-07-22 18:31:11 +0000

    grader hw3 part3 updates

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,11 +64,35 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'Url to check')
+       .parse(process.argv);
+    
+    var checkJson;
+
+    // if url argument is present, use it
+    if (program.url != undefined) {
+        rest.get(program.url).on("complete", function(data, response) {
+            checkJson = checkHtmlFile(cheerio.load(data), program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        });
+    // else if file argument is present, use it
+    } else if (program.file != undefined){    
+        var file = assertFileExists(program.file);
+        checkJson = checkHtmlFile(cheerioHtmlFile(file), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    // else use default file
+    } else {
+        var file = assertFileExists(HTMLFILE_DEFAULT);
+        checkJson = checkHtmlFile(cheerioHtmlFile(file), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
